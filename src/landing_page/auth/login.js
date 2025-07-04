3585cd2dc726797aecfafc40bbce5c7bce46aa78
akshat9463 <akshatkhadi@gmail.com>
2025-07-04 20:49:30 +0530

    all routes fixed

--- src/landing_page/auth/login.js
+++ src/landing_page/auth/login.js
@@ -43,7 +43,7 @@ const Login = () => {
       if (success) {
         handleSuccess(message);
         setTimeout(() => {
-          window.location.href = "http://localhost:3001";
+          window.location.href = "https://zerodha-dashboard-uu4u.onrender.com";
         }, 1000);
       } else {
         handleError(message);
